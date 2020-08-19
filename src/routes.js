62d3288ecf2eb90245bde8c0b571a474f178f4a8
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-19 17:52:10 -0300

    router configurations

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+import React from 'react';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+}
+
+export default Routes;
