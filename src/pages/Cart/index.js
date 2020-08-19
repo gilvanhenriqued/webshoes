5b187167e3351ed3f985ed6c96070f114bc542cd
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-19 19:32:15 -0300

    cart component structure and styles

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ts=1571078789&" alt="Produto"/>
+            </td>
+            <td>
+              <strong>Tênis muito massinha</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={15} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+    
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1922,90</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
