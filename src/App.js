3ba15feb4aed78d0f6ba36d6bd81d117fbdafe76
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-20 09:54:53 -0300

    reactotron debug configurations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './config/Reactotron.config';
 import store from './store';
 
 import Routes from './routes';
